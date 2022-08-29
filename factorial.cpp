66e8d56c688d8abb32099b9eab9270996f2c7cc8
cs-308 <dishtioberai@gmail.com>
2022-08-29 15:53:17 +0530

    Commiting code - Iteration 1

---
+++ factorial.cpp
@@ -0,0 +1,12 @@
+#include "functions.h"
+
+int factorial(int n){
+	if(n<0){cout<<"Enter a positive number"<<endl;
+	int p;
+	cin>>p;
+	n=p;}
+    	else if(n!=1){
+		return(n * factorial(n-1));
+    	}
+    	else return 1;
+}
