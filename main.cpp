a19ed980ffcf69f475432906fd54ab0ccfe285a0
lazyXghost <rjtbnslrealme@gmail.com>
2022-09-06 08:30:35 +0530

    Added code to print gcd recursively

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main(){
     cout<<"The factorial of 5 is "<<factorial(5)<<endl;
     int x = 56, y = 98;
     cout<<"GCD of ("<<x<<","<<y<<") is "<< gcd(x, y)<<endl;
+    cout<<"GCD of ("<<x<<","<<y<<") using recursive function is "<< gcdRecursive(x, y)<<endl;
     return 0;
 }
